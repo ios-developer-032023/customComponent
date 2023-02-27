3aad3f5050c042570d77491452d8c7a99562d75d
iOS-developer-032023 <ios.developer.032023@gmail.com>
2023-02-27 12:16:24 +0530

    Initial project setup

---
+++ customComponent/customComponent.h
@@ -0,0 +1,18 @@
+//
+//  customComponent.h
+//  customComponent
+//
+//  Created by Jignesh Ashara on 27/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for customComponent.
+FOUNDATION_EXPORT double customComponentVersionNumber;
+
+//! Project version string for customComponent.
+FOUNDATION_EXPORT const unsigned char customComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <customComponent/PublicHeader.h>
+
+
---
+++ customComponentExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customComponentExample
+//
+//  Created by Jignesh Ashara on 27/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
