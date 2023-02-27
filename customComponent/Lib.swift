52111952a86360ef1aaf833807696b3f700f4f9a
iOS-developer-032023 <ios.developer.032023@gmail.com>
2023-02-27 13:48:15 +0530

    Added code n test cases

---
+++ customComponent/Lib.swift
@@ -0,0 +1,22 @@
+//
+//  Lib.swift
+//  customComponent
+//
+//  Created by Jignesh Ashara on 27/02/23.
+//
+
+import Foundation
+
+public final class Lib {
+
+    let name = "Lib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
